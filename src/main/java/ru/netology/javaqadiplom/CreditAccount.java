4b13ab803acf6f8a38c97296741cdfb25f94e39f
Shabrov A.L. QA-72 <valmar.chab@gmail.com>
2023-11-18 15:43:04 +0500

    TestRedact4

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -52,7 +52,7 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance - amount > -creditLimit) {
+        if (balance - amount >= -creditLimit) {
             balance = balance - amount;
             return true;
         } else {
